68ea8f4c0dcabcc68ef2cdb67ec4f31b3fa173a6
xzxgit <m13694555712@163.com>
2018-05-27 12:39:05 +0800

    [ADD] add manager view

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,3 +1,35 @@
 <template>
-    <h1>Admin</h1>
+    <div class="row">
+        <div class="col-sm-12 col-md-8">
+            <app-new-pizza></app-new-pizza>
+        </div>
+        <div class="sm-12 col-md-4">
+            <h3 class="text-muted my-5">Menu</h3>
+            <table class="table table-default">
+                <thead>
+                    <tr>
+                        <th>Category</th>
+                        <td>Delete</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>sadad</td>
+                        <td>
+                            <button class="btn btn-sm btn-outline-danger">&times;</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
+
+<script>
+import NewPizza from './NewPizza.vue'
+export default {
+    components: {
+        'app-new-pizza': NewPizza
+    }
+}
+</script>
---
+++ src/components/NewPizza.vue
@@ -0,0 +1,59 @@
+<template>
+    <form>
+        <h3 class="text-muted my-5">Add New Pizza</h3>
+        <div class="form-group row">
+            <label class="col-sm-2">Category</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" v-model="newPizza.name">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2">Description</label>
+            <div class="col-sm-10">
+                <textarea rows="5" class="form-control" v-model="newPizza.description"></textarea>
+            </div>
+        </div>
+        <p class="strong">Option One</p>
+        <div class="form-group row">
+            <label class="col-sm-2">Size</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" v-model="newPizza.size1">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2">Price</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" v-model="newPizza.price1">
+            </div>
+        </div>
+
+        <p class="strong">Option Two</p>
+        <div class="form-group row">
+            <label class="col-sm-2">Size</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" v-model="newPizza.size2">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2">Price</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" v-model="newPizza.price2">
+            </div>
+        </div>
+        <div class="form-group row">
+            <button type="button" class="btn btn-success btn-block">Add</button>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                newPizza: {
+
+                }
+            }
+        }
+    }
+</script>
